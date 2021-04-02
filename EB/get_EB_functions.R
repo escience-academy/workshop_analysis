2d32ac4787011415cbfb5e20cbc5aa88f8defcf2
= <=>
2021-04-02 12:36:19 +0200

    load functions needed for EB script

---
+++ EB/get_EB_functions.R
@@ -0,0 +1,10 @@
+get_EB_functions <- function() {
+  
+  source('get_ppt_info.R')
+  source('event_info.R')
+  source('get_answers.R')
+  source('cut_disciplines.R')
+  source('cut_career_stage.R')
+  
+  
+}
