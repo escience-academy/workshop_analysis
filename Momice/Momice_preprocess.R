2b0d25eb203fe730727dfabedd0e712741d12459
= <=>
2021-05-25 13:15:57 +0200

    add private info to gitignore, start with momice preprocessing

---
+++ Momice/Momice_preprocess.R
@@ -0,0 +1,6 @@
+Momice_disc <- c("Agricultural or Environmental Sciences", "Biomedical or Health Sciences", "Chemistry", "Civil, Mechanical, Chemical, or Nuclear Engineering ", 
+                 "Computer Science or Electrical Engineering ","Earth Sciences", "Genetics, Genomics or Bioinformatics", "High Performance Computing", 
+                 "Humanities", "Planetary Sciences (Geology, Climatology, Oceanography, etc.)","Economics and business", "Education", 
+                 "Library and information sciences", "Life Sciences", "Mathematics or Statistics", "Medicine","Physical Sciences", 
+                 "Psychology or Neuroscience", "Organismal biology (Ecology, Botany, Zoology, Microbiology, etc.)", "Social Sciences",
+                 "Space Sciences", "Not Applicable")
