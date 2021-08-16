0b506f51d093292081034d72ed8ee2f3d466b6b4
Maaike <maaike@M.local>
2021-08-16 11:30:22 +0200

    Maaike commits

--- Momice/Momice_preprocess.R
+++ Momice/Momice_preprocess.R
@@ -1,6 +1,24 @@
+library(tidyverse)
+library(rio)
+
+exec_dir <- dirname(rstudioapi::getSourceEditorContext()$path) #the dir this script is in
+setwd(exec_dir)
+
+
 Momice_disc <- c("Agricultural or Environmental Sciences", "Biomedical or Health Sciences", "Chemistry", "Civil, Mechanical, Chemical, or Nuclear Engineering ", 
                  "Computer Science or Electrical Engineering ","Earth Sciences", "Genetics, Genomics or Bioinformatics", "High Performance Computing", 
                  "Humanities", "Planetary Sciences (Geology, Climatology, Oceanography, etc.)","Economics and business", "Education", 
                  "Library and information sciences", "Life Sciences", "Mathematics or Statistics", "Medicine","Physical Sciences", 
                  "Psychology or Neuroscience", "Organismal biology (Ecology, Botany, Zoology, Microbiology, etc.)", "Social Sciences",
                  "Space Sciences", "Not Applicable")
+
+swc.R.20210517 <- rio::import(paste0(data_dir, "2021-05-17-swc-R-nlesc-Registrations.xlsx"))
+names(swc.R.20210517)[7:28] <- Momice_disc %>% 
+  mutate()
+
+
+data_dir <- paste0(dirname(exec_dir),'/data/')
+
+
+
+
